f50b15af8aa03184ff97d16b92afa257175db5fa
Tomas Linan <tomas.m.l.001@gmail.com>
2022-04-06 16:15:07 -0300

    ShowCase Finished

---
+++ src/components/Download.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import styles from '../styles/Global'
+import assests from '../assets'
+
+const Download = ({ link }) => {
+  return (
+    <div className={`${styles.section} ${styles.bgWhite}`}>
+      <div className={`${styles.subSection} flex-col text-center`}>
+        <div>
+          <h1 className={`${styles.h1Text} ${styles.blackText}`}>Download the Source Code</h1>
+          <p className={`${styles.pText} ${styles.blackText}`}>Get the full source code on GitHub</p>
+          <button 
+            className={styles.btnPrimary} 
+            onClick={() => window.open(link, "_blank")}>
+              Source Code
+          </button>
+          <div className={styles.flexCenter}>
+            <img src={assests.scene} alt="download_png" className={styles.fullImg}/>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Download
